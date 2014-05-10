07c9f6b74c5064529e281d20a168aef7be778d48
Atsushi Odagiri <aodagx@gmail.com>
2014-05-10 18:28:24 +0900

    added layout

--- bookmatch/layouts.py
+++ bookmatch/layouts.py
@@ -1,1 +1,10 @@
 # -*- coding:utf-8 -*-
+
+from pyramid_layout.layout import layout_config
+
+
+@layout_config(template="templates/base_layout.html")
+class BaseLayout(object):
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
